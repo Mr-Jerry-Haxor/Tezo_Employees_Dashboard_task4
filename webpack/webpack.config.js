1be1c0149f3971218dbe5800fe3a71440153c803
SHRAVAN JERIPOTHULA <98485154+Mr-Jerry-Haxor@users.noreply.github.com>
2024-03-05 16:05:44 +0530

    old files removed

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,24 +1,25 @@
 const path = require('path');
 
 module.exports = {
-  entry: {
-    include: ["./TypeScript/index.ts" , "./TypeScript/addemployee.ts" , "./TypeScript/employee.ts" , "./TypeScript/data.ts"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: "ts-loader",
-        include: [path.resolve(__dirname, 'TypeScript')],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'bundle'),
-  },
+    mode: 'development',
+    entry: {
+        include: ["./TypeScript/index.ts" , "./TypeScript/addemployee.ts" , "./TypeScript/employee.ts" , "./TypeScript/data.ts"],
+    },
+    module: {
+        rules: [
+        {
+            test: /\.ts$/,
+            exclude: /node_modules/,
+            use: "ts-loader",
+            include: [path.resolve(__dirname, 'TypeScript')],
+        },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'bundle'),
+    },
 };
